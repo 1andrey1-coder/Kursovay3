e2310b63f6d05c8766415503f076bc7cc959daaf
windows <windows@LAPTOP-2R2MGI84>
2024-02-07 19:33:48 +1000

    vved

---
+++ Kursovay2/Static/PageNavigator.cs
@@ -0,0 +1,27 @@
+﻿using Kursovay2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Kursovay2.Static
+{
+    internal class PageNavigator
+    {
+        private static ClassWindow currentPage;
+
+        public static ClassWindow CurrentPage
+        {
+            get => currentPage;
+            set
+            {
+                currentPage = value;
+                CurrentPageChanged?.Invoke(null, currentPage);
+            }
+        }
+
+        public static event EventHandler<ClassWindow> CurrentPageChanged;
+    }
+}
---
+++ Kursovay2/mvvm/CommandVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Kursovay2.mvvm
+{
+    public class CommandVM : ICommand
+    {
+
+        private readonly Action action;
+        private readonly Func<bool> condition;
+
+        public CommandVM(Action action,
+            Func<bool> condition)
+        {
+            this.action = action;
+            this.condition = condition;
+        }
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return condition();
+        }
+
+        public void Execute(object? parameter)
+        {
+            action();
+        }
+    }
+}
