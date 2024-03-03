0ca627a8f71ebb5f55437fa6eaf5e00141933026
windows <windows@LAPTOP-2R2MGI84>
2024-03-03 17:38:02 +1000

    Регистрация с присвоением роли только для новых user

--- Kursovay2Api2.0/Kursovay2Api2.0/Controllers/AccountController.cs
+++ Kursovay2Api2.0/Kursovay2Api2.0/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using ApiDB.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kursovay2Api2._0.Controllers
 {
@@ -37,6 +38,57 @@ namespace Kursovay2Api2._0.Controllers
             }
 
         }
+        [HttpPost("Register")]
+        public async Task<ActionResult<LoginUserDTO>> GetActionRegister(RegisterDTO registerUser)
+        {
+            try
+            {
+                // Проверка наличия пользователя с таким логином
+                var ProverkaUser = await _memContext.LoginUsers.
+                    FirstOrDefaultAsync(u => u.LoginName == registerUser.Login);
 
+                if (ProverkaUser != null)
+                {
+                    return BadRequest("Пользователь с таким логином уже существует");
+                }
+                else
+                {
+                    // Создание нового объекта LoginUser и добавление его в контекст
+                    var newUser = new LoginUser
+                    {
+                        LoginId = registerUser.Id,
+                        LoginName = registerUser.Login,
+                        LoginPassword = registerUser.Password,
+                        RoleId = 2
+
+                    };
+
+
+                    _memContext.LoginUsers.Add(newUser);
+                    await _memContext.SaveChangesAsync();
+
+                    //Возвращаем созданный объект в виде DTO для отправки ответа клиенту
+                    var loginUserDTO = new LoginUserDTO
+                    {
+                        LoginId = newUser.LoginId,
+                        LoginName = newUser.LoginName,
+                        LoginPassword = newUser.LoginPassword,
+                        RoleId = newUser.RoleId
+                    };
+
+                    return Ok(loginUserDTO);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, "Произошла ошибка при регистрации пользователя");
+            }
+
+
+            //return null;
+        }
     }
 }
