5c237873b8519960a3a03d7c762e52bb274d3497
Student <Student@CAB4-10>
2024-04-01 15:10:21 +1000

    emailsenderSMTP

--- Kursovay2Api2.0/Kursovay2Api2.0/Controllers/AccountController.cs
+++ Kursovay2Api2.0/Kursovay2Api2.0/Controllers/AccountController.cs
@@ -166,7 +166,8 @@ namespace Kursovay2Api2._0.Controllers
                 //СингТон
 
 
-                MailMessage message = EmailMessageService.sender.CreateMailMessageBodyIsText("Словарь сленга", registerUser.Mail, "Регистрация", $"Ваш пароль: {password}");
+                MailMessage message = EmailMessageService.sender.CreateMailMessageBodyIsText
+                    ("Словарь сленга", registerUser.Mail, "Регистрация", $"Ваш пароль: {password}");
                 EmailMessageService.sender.SendMail(message);
                 
                 //await mail.Send("ilchenkor17@mail.ru", registerUser.Mail, "Регистрация", $"Ваш пароль: {password}");
