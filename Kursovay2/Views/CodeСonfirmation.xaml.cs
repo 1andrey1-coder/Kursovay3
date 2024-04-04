ddebd33a4abad8e5ebb214582ef0d2527238e37f
windows <windows@LAPTOP-2R2MGI84>
2024-04-04 20:21:47 +1000

    ff

--- Kursovay2/Views/CodeСonfirmation.xaml.cs
+++ Kursovay2/Views/CodeСonfirmation.xaml.cs
@@ -64,5 +64,15 @@ namespace Kursovay2.Views
                 MessageBox.Show("Неверный код");
             }
         }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Window parentWindow = Window.GetWindow(this);
+            parentWindow.WindowState = WindowState.Minimized;
+        }
     }
 }
