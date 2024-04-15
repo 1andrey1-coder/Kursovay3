b72900882c71032afea86574ee63ebabf6cc939b
windows <windows@LAPTOP-2R2MGI84>
2024-04-15 18:40:05 +1000

    па

--- Kursovay2Api2.0/ApiDB/Program.cs
+++ Kursovay2Api2.0/ApiDB/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//("server=192.168.200.35;password=64457;user=user43;database=user43;TrustServerCertificate=true"); колледж
+//("server=LAPTOP-2R2MGI84\\SQLEXPRESS;password=admin;user=admin;database=mem;TrustServerCertificate=true"); дома
