d7a5bafea814cd54aaf064919be36632d2232ad4
windows <windows@LAPTOP-2R2MGI84>
2024-04-15 10:18:03 +1000

    dfd

--- Kursovay2/API/Client.cs
+++ Kursovay2/API/Client.cs
@@ -198,7 +198,7 @@ namespace Kursovay2.API
 
 
         //ilchenkor1135@suz-ppk.ru
-        //YPwYBwyp Дома
+        //6qRSZY9Y Дома
         //eHzxEAGf колледж
         //Failed to load data from API
         public async Task<List<RoflDTO>> GetListRofl()
